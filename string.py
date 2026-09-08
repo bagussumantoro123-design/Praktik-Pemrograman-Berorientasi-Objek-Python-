94f4f9d4128fa4d4bd512c75a49c3482ada5acf4
bagussumantoro123-design <bagussumantoro123@gmail.com>
2026-09-08 17:41:29 +0700

    Add files via upload
    Kode Python pada tipe data string (str)

---
+++ string.py
@@ -0,0 +1,16 @@
+# string sebaris
+string_1 = "hello python"
+
+# string multi_baris
+string_2 = """Selamat 
+Belajar 
+Python"""
+
+# string sebaris
+string_3 = 'for the horde!'
+
+# string multi-baris
+string_4 = '''
+Besok
+Libur
+'''
